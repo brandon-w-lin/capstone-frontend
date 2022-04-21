6e07af3c8ff4f64ebefbc98e6d5af10eafefa9a8
Brandon Lin <bwlin93@gmail.com>
2022-04-21 14:59:24 -0500

    adds redirects to index and edit pages

--- src/views/BooksShow.vue
+++ src/views/BooksShow.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">Books show</div>
-  {{ book }}
+  <div>
+    {{ book }}
+  </div>
+  <div>
+    <button @click="redirectBooksIndex()">Back to all books</button>
+    <button @click="redirectBooksEdit()">Edit Book</button>
+  </div>
 </template>
 
 <script>
@@ -18,6 +24,10 @@ export default {
     redirectBooksIndex: function () {
       console.log("Redirecting to /books");
       this.$router.push("/books");
+    },
+    redirectBooksEdit: function () {
+      console.log("Redirecting to /books/" + this.$route.params.id + "/edit");
+      this.$router.push("/books/" + this.$route.params.id + "/edit");
     },
     showBook: function () {
       axios.get("http://localhost:3000/books/" + this.$route.params.id + ".json").then((response) => {
