3765233a723c2473801e725b45643492ed4f26d4
Brandon Lin <bwlin93@gmail.com>
2022-04-22 17:05:37 -0500

    adds cards first draft

--- src/views/BooksIndex.vue
+++ src/views/BooksIndex.vue
@@ -1,22 +1,53 @@
 <template>
-  <div class="home">
+  <div class="container">
     <h1>All books</h1>
-    <div v-for="book in books" :key="book.id">
-      {{ book }}
-      <button @click="redirectBooksShow(book)">More Info</button>
+
+    <!-- cards not in use -->
+    <!-- <div class="row">
+      <div class="col-sm-4 mb-2" v-for="book in books" :key="book.id">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ book.title }}</h5>
+            <p class="card-text">Brief blurb about the book</p>
+            <a :href="'/books/' + book.id" class="btn btn-primary">More Info</a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- <div class="card mb-3" style="max-width: 540px" v-for="book in books" :key="book.id"> -->
+
+    <!-- cards in use -->
+    <div class="row">
+      <div class="card m-3" style="max-width: 540px" v-for="book in books" :key="book.id">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img :src="book.cover_image_url" class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ book.title }}</h5>
+              <p class="card-text">
+                {{ book.summary.substring(0, charLimit) + (book.summary.length > charLimit ? "..." : "") }}
+              </p>
+              <a :href="'/books/' + book.id" class="btn btn-primary">More Info</a>
+
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
 import axios from "axios";
 
 export default {
   data: function () {
     return {
       books: [],
+      charLimit: 200,
     };
   },
   methods: {
