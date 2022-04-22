f27524a583edcc91964f427f84a520e2d9fcacac
Brandon Lin <bwlin93@gmail.com>
2022-04-22 17:59:20 -0500

    formats: header, adds cards, refactors redirect methods to use hrefs instead

--- src/views/BooksNew.vue
+++ src/views/BooksNew.vue
@@ -1,45 +1,52 @@
 <template>
-  <div class="home">Books New</div>
-  <div>
-    <form>
-      <p>
-        <span>ISBN:</span>
-        <input type="text" v-model="book.isbn" placeholder="ISBN" />
-      </p>
-      <p>
-        <span>Title:</span>
-        <input type="text" v-model="book.title" placeholder="Title" />
-      </p>
-      <p>
-        <span>Summary:</span>
-        <input type="text" v-model="book.summary" placeholder="Summary" />
-      </p>
-      <p>
-        <span>Author:</span>
-        <input type="text" v-model="book.author" placeholder="Author" />
-      </p>
-      <p>
-        <span>Publisher:</span>
-        <input type="text" v-model="book.publisher" placeholder="Publisher" />
-      </p>
-      <p>
-        <span>URL to cover image:</span>
-        <input type="text" v-model="book.cover_image_url" placeholder="URL of image cover" />
-      </p>
-      <p>
-        <span>Genre:</span>
-        <input type="text" v-model="book.genre" placeholder="Genre" />
-      </p>
-      <p>
-        <span>Year published:</span>
-        <input type="text" v-model="book.year" placeholder="Year" />
-      </p>
-    </form>
-    <button @click="createBook()">Submit</button>
+  <div class="card m-1">
+    <div class="card-body">
+      <h1>Submit a new book</h1>
+    </div>
   </div>
-  <p></p>
-  <div>
-    <button @click="redirectBooksIndex()">Back to all books</button>
+  <div class="card m-1">
+    <div class="card-body">
+      <form>
+        <p>
+          <span>ISBN:</span>
+          <input type="text" v-model="book.isbn" placeholder="ISBN" />
+        </p>
+        <p>
+          <span>Title:</span>
+          <input type="text" v-model="book.title" placeholder="Title" />
+        </p>
+        <p>
+          <span>Summary:</span>
+          <input type="text" v-model="book.summary" placeholder="Summary" />
+        </p>
+        <p>
+          <span>Author:</span>
+          <input type="text" v-model="book.author" placeholder="Author" />
+        </p>
+        <p>
+          <span>Publisher:</span>
+          <input type="text" v-model="book.publisher" placeholder="Publisher" />
+        </p>
+        <p>
+          <span>URL to cover image:</span>
+          <input type="text" v-model="book.cover_image_url" placeholder="URL of image cover" />
+        </p>
+        <p>
+          <span>Genre:</span>
+          <input type="text" v-model="book.genre" placeholder="Genre" />
+        </p>
+        <p>
+          <span>Year published:</span>
+          <input type="text" v-model="book.year" placeholder="Year" />
+        </p>
+      </form>
+      <button @click="createBook()" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+  <div class="card m-1">
+    <div class="card-body">
+      <a href="./" class="btn btn-primary">Back to all books</a>
+    </div>
   </div>
 </template>
 
@@ -55,14 +62,6 @@ export default {
     };
   },
   methods: {
-    redirectBooksIndex: function () {
-      console.log("Redirecting to /books");
-      this.$router.push("/books");
-    },
-    redirectBooksShow: function () {
-      console.log("Redirecting to /books/" + this.$route.params.id);
-      this.$router.push("/books/" + this.$route.params.id);
-    },
     createBook: function () {
       axios.post("http://localhost:3000/books.json", this.book).then((response) => {
         console.log("successfully submitted book", response.data);
