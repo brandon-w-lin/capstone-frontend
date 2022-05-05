d7f08a795a5570afa36b51625edeae8c7e9ac2d4
Brandon Lin <bwlin93@gmail.com>
2022-05-04 21:25:10 -0500

    reformats the show page to have book section alongside top songs section

--- src/views/BooksShow.vue
+++ src/views/BooksShow.vue
@@ -1,30 +1,57 @@
 <template>
-  <div>
-    <div class="column">test left</div>
-    <div class="column">test right</div>
-  </div>
-  <div class="card m-1">
-    <div class="card-body">
-      <h2 class="card-subtitle text-muted">
-        <i>{{ book.subtitle }}</i>
-      </h2>
-      <h1 class="card-title">
-        {{ book.title }}
-      </h1>
-      <h3>{{ book.authors.join(", ") }}</h3>
-      <div>{{ book.publisher }}</div>
-      <div>{{ book.publishedDate }}</div>
-      <div v-html="book.description"></div>
-      <div>{{ book.categories[0] }}</div>
-      <div v-for="identifier in book.industryIdentifiers" :key="identifier.type">
-        {{ `${identifier.type}: ${identifier.identifier}` }}
+  <div class="container">
+    <div class="row">
+      <!-- Left side of page holding book info -->
+      <div class="col-sm-6">
+        <!-- holds book info -->
+        <div class="card p-3 m-2">
+          <!-- This row holds the picture and main book info -->
+          <div class="row mb-2">
+            <!-- Picture -->
+            <div class="col-auto">
+              <img :src="book.imageLinks.thumbnail" class="img-fluid img-thumbnail rounded-start" alt="..." />
+            </div>
+
+            <!-- Book Info -->
+            <div class="col-auto text-left align-middle">
+              <div class="row card-subtitle text-muted">
+                <i>{{ book.subtitle }}</i>
+              </div>
+              <div class="row h1 card-title">
+                {{ book.title }}
+              </div>
+              <div class="row h3">{{ book.authors.join(", ") }}</div>
+              <div class="row">{{ book.publisher }}</div>
+              <div class="row">{{ book.publishedDate }}</div>
+              <div class="row">{{ book.categories[0] }}</div>
+              <div class="row" v-for="identifier in book.industryIdentifiers" :key="identifier.type">
+                {{ `${identifier.type}: ${identifier.identifier}` }}
+              </div>
+            </div>
+          </div>
+
+          <!-- Everything below the main book info (book description) -->
+          <div class="row">
+            <div class="col-auto" v-html="book.description"></div>
+          </div>
+        </div>
+
+        <!-- holds buttons -->
+        <div class="card m-2">
+          <div class="row">
+            <div class="col">Back to all books</div>
+            <div class="col">Submit new song</div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="card m-1">
-    <div class="card-body">
-      <a href="./" class="btn btn-primary">Back to all books</a>
-      <a :href="'./books/' + this.$route.params.id + '/edit'" class="btn btn-primary">Edit</a>
+
+      <!-- Right side of page holding top songs -->
+      <div class="col-sm-5 m-2">
+        <div class="row card mb-2 h2">Most played songs for {{ book.title }}</div>
+        <div class="row card mb-2">Song 1</div>
+        <div class="row card mb-2">Song 2</div>
+        <div class="row card mb-2">Song 3</div>
+      </div>
     </div>
   </div>
 </template>
