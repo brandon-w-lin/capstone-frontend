bf3c079d061bd7170ddf02e2f4e5dcb2ae3eab12
Brandon Lin <bwlin93@gmail.com>
2022-05-21 16:32:12 -0500

    formatting of cards: image hover, box shadow, 3 cols at xl 2 at lg 1 else

--- src/views/BooksIndex.vue
+++ src/views/BooksIndex.vue
@@ -5,29 +5,26 @@
 
   <div class="row">
     <!-- Padding added to the column before adding the card -->
-    <div class="col-6 p-3" v-for="book in books" :key="book.id">
-      <div class="card">
-        <div class="row">
-          <div class="col-sm-4 p-1 cover-image">
-            <img
-              :src="
-                book.imageLinks
-                  ? book.imageLinks.thumbnail || Object.values(book.imageLinks).pop()
-                  : 'https://www.seekpng.com/png/full/96-965662_confused-travolta-pulp-fiction-side-john-travolta-confused.png'
-              "
-              class="img-fluid img-thumbnail rounded-start"
-              alt=""
-            />
-          </div>
-          <div class="col-sm-8">
-            <div class="card-body text-left">
-              <h5 class="card-title">{{ book.title }}</h5>
-              <h6 class="card-subtitle">
-                {{ book.authors ? book.authors.join(", ") : "Author(s) unknown" }}
-              </h6>
-              <router-link :to="/books/ + book.id" class="btn btn-primary">More Info</router-link>
-              <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
-            </div>
+    <div class="col-xl-4 col-lg-6 p-3" v-for="book in books" :key="book.id">
+      <div class="card book selected">
+        <!-- <div class="col-sm-4 cover-image"> -->
+        <img
+          :src="
+            book.imageLinks
+              ? book.imageLinks.thumbnail || Object.values(book.imageLinks).pop()
+              : 'https://www.seekpng.com/png/full/96-965662_confused-travolta-pulp-fiction-side-john-travolta-confused.png'
+          "
+          class="img-fluid float-left zoom"
+          alt=""
+        />
+        <div>
+          <div class="card-body text-left">
+            <h5 class="card-title">{{ book.title }}</h5>
+            <h6 class="card-subtitle">
+              {{ book.authors ? book.authors.join(", ") : "Author(s) unknown" }}
+            </h6>
+            <router-link :to="/books/ + book.id" class="btn btn-primary">More Info</router-link>
+            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
           </div>
         </div>
       </div>
@@ -79,7 +76,34 @@ export default {
 </script>
 
 <style>
+.card {
+  border: none;
+}
 .centered {
   margin: 0 auto;
+}
+
+.img-container {
+  object-fit: cover;
+}
+
+.book {
+  flex-direction: row;
+  height: 175px;
+  overflow: hidden;
+  box-shadow: 0px 0px 8px 1px rgba(0, 0, 0, 0.6);
+  transition: 0.4s;
+}
+
+.book:hover {
+  box-shadow: 0px 0px 12px 2px rgba(172, 170, 244, 0.7);
+}
+
+.book img {
+  transition: transform 0.2s;
+}
+
+.book:hover img {
+  transform: scale(1.1);
 }
 </style>
