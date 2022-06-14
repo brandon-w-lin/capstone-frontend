11e858d7273db2daae3b43f16c87cbbf94518f6d
Brandon Lin <bwlin93@gmail.com>
2022-06-14 16:45:12 -0500

    adds upnext button -> bootstrap collapse with upNext.title

--- src/components/AudioComponent.vue
+++ src/components/AudioComponent.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="player">
     <div class="container">
+      <div class="collapse" id="collapseWidthExample">
+        <div class="card card-body" style="width: 300px">
+          <div v-for="song in upNext" :key="song.YT_extension">
+            {{ song.title }}
+          </div>
+        </div>
+      </div>
       <div class="row">
         <!-- Navigation buttons -->
         <div class="col-auto my-1 p-1 d-flex align-items-center">
@@ -63,6 +70,27 @@
             />
             <span>{{ formatTime(this.duration) }}</span>
           </div>
+        </div>
+        <div class="col-auto m-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            class="clickable"
+            viewBox="0 0 16 16"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseWidthExample"
+            aria-expanded="false"
+            aria-controls="collapseWidthExample"
+          >
+            <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
+            <path fill-rule="evenodd" d="M12 3v10h-1V3h1z" />
+            <path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1V2.82z" />
+            <path
+              fill-rule="evenodd"
+              d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
         </div>
       </div>
     </div>
