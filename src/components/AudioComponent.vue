55138904f603b439a9a0c9c5326e91f99efab2d6
Brandon Lin <bwlin93@gmail.com>
2022-06-16 14:49:16 -0500

    fixes bug with interval polling of the video progress for scroll bar => update the current time whenever scrolling => scroll bar feels snappy

--- src/components/AudioComponent.vue
+++ src/components/AudioComponent.vue
@@ -197,14 +197,14 @@ export default {
     trackTime() {
       this.currentTime = this.YTplayer.getCurrentTime();
       this.formattedCurrentTime = this.formatTime(this.currentTime);
-      // console.log(this.currentTime);
-      console.log(this.duration - this.currentTime);
       if (this.duration - this.currentTime < 1) {
         this.nextSong();
       }
     },
     scrollTo() {
       this.YTplayer.seekTo(document.getElementById("progressBar").value, true);
+      this.currentTime = document.getElementById("progressBar").value;
+      this.formattedCurrentTime = this.formatTime(document.getElementById("progressBar").value);
     },
 
     selectedSongController(song, upNext) {
